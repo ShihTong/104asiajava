5be6cb2ab4c313e49610fb3c2ecbf02111c4eb54
asdasd950 <o60417o@yahoo.com.tw>
2015-08-03 14:10:36 +0800

    V2.0

--- src/minion.java
+++ src/minion.java
@@ -6,20 +6,20 @@ public class minion {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.out.println("　　　╭───────────╮");
-    System.out.println("　　　│　　　　　　　　　　　│");
-    System.out.println("　　　│　　　　　　　　　　　│");
-    System.out.println("　　　╞═════⊙═⊙════╡");
-    System.out.println("　　　│　　　　　　　　　　　│");
-    System.out.println("　　　│　　　　　　　　　　　│");
-    System.out.println("　　　│　　　　　▽　　　　　│");
-    System.out.println("　╭─│　　　　　　　　　　　│─╮");
-    System.out.println("　│　│　　　　　　　　　　　│　　│");
-    System.out.println("　│　│███████████████│　│");
-    System.out.println("　│　│███████████████│　│");
-    System.out.println("　██　███████████████　██");
-    System.out.println("　　　   ██████   ██████");
-    System.out.println("　　　　╰───╯　╰───╯");
+    System.out.println("　　　╭─────────────╮");
+    System.out.println("　　　│　　　　　  　│");
+    System.out.println("　　　│　　　　　  　│");
+    System.out.println("　　　╞═════⊙═⊙═════╡");
+    System.out.println("　　　│　　　　  　　│");
+    System.out.println("　　　│　　　　  　　│");
+    System.out.println("　　　│　　　▽  　　 │");
+    System.out.println("　 ╭─│　　　　  　　 │ ╮"");
+    System.out.println("　 │　│　　　　　  　 │ │");
+    System.out.println("　 │　│███████████████│ │");
+    System.out.println(" 　│　│███████████████│ │");
+    System.out.println(" 　██　███████████████ ██");
+    System.out.println("　　　  █████    █████");
+    System.out.println("　　 　╰───╯ 　╰───╯");
     System.out.println("");
     System.out.println("");
     System.out.println("");
