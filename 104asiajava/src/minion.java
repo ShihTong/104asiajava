4615e8c1574e2d2722e085fe1a05b47cb5dd6abb
asdasd950 <o60417o@yahoo.com.tw>
2015-08-04 09:39:02 +0800

    V1.0

--- 104asiajava/src/minion.java
+++ 104asiajava/src/minion.java
@@ -13,7 +13,7 @@ public class minion {
     System.out.println("　　　│　　　　  　　│");
     System.out.println("　　　│　　　　  　　│");
     System.out.println("　　　│　　　▽  　　 │");
-    System.out.println("　 ╭─│　　　　  　　 │ ╮"");
+    System.out.println("　 ╭─│　　　　  　　 │ ╮");
     System.out.println("　 │　│　　　　　  　 │ │");
     System.out.println("　 │　│███████████████│ │");
     System.out.println(" 　│　│███████████████│ │");
@@ -31,3 +31,4 @@ public class minion {
     }
 
 }
+
