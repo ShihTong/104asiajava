ec36428e4608705e09a96aa84644e968fa710584
asdasd950 <o60417o@yahoo.com.tw>
2015-08-04 09:40:41 +0800

    Update minion.java

--- 104asiajava/src/minion.java
+++ 104asiajava/src/minion.java
@@ -6,13 +6,13 @@ public class minion {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.out.println("　　　╭────────────╮");
-    System.out.println("　　　│　　　　　  　│");
-    System.out.println("　　　│　　　　　  　│");
-    System.out.println("　　　╞═════⊙═⊙════╡");
-    System.out.println("　　　│　　　　  　　│");
-    System.out.println("　　　│　　　　  　　│");
-    System.out.println("　　　│　　　▽  　　 │");
+    System.out.println("　　　╭──────────────╮");
+    System.out.println("　　　│　　　　　    　│");
+    System.out.println("　　　│　　　　　    　│");
+    System.out.println("　　　╞═════⊙═⊙═══════╡");
+    System.out.println("　　　│　　　　  　  　│");
+    System.out.println("　　　│　　　　  　  　│");
+    System.out.println("　　　│　　　▽  　　  │");
     System.out.println("　 ╭─│　　　　  　　  │ ╮");
     System.out.println("　 │　│　　　　　  　  │ │");
     System.out.println("　 │　│███████████████│ │");
