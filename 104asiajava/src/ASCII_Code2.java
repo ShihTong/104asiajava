04825d9dab2523e780412507b2e309a7ce1fba75
asdasd950 <o60417o@yahoo.com.tw>
2015-08-05 16:14:53 +0800

    A

--- 104asiajava/src/ASCII_Code2.java
+++ 104asiajava/src/ASCII_Code2.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-public class ASCII_Code {
-
+public class ASCII_Code2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scn = new  Scanner(System.in);
@@ -9,17 +8,22 @@ public class ASCII_Code {
 		char[] ch = str.toCharArray();
 		for(int i=0;i<ch.length;i++){
 			if(ch[i]>=97 && ch[i]<=122){
-				String s =String.valueOf(ch[i]);
-				System.out.print(s.toUpperCase());
-				
+				System.out.print("小寫");
+				break;
 			}else{
 				if(ch[i]>=65 && ch[i]<=90){
-					String s =String.valueOf(ch[i]);
-					System.out.print(s.toLowerCase());
-				}
+					System.out.print("大寫");
+					break;
+		}else{
+		if(ch[i]!=65 && ch[i]!=90){
+			System.out.print("其他字元");
+			break;
+		}
+		}
+		}
+			
+				
+			}
+        }
 		}
 
-	}
-
-}
-}
---
+++ 104asiajava/src/Score2.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+public class Score2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new  Scanner(System.in);
+		System.out.println("Enter your score.");
+		float a =scn.nextFloat();
+		if(a>97){
+			System.out.println("Your grade is A+.");
+	}else{
+		if(a<=97&&a>=94){
+			System.out.println("Your grade is A.");
+	}else{
+		if(a<=94&&a>91){
+			System.out.println("Your grade is A-.");
+	}else{
+	}
+		if(a<=91&&a>88){
+			System.out.println("Your grade is B+.");
+	}else{
+	}
+		if(a<=88&&a>85){
+			System.out.println("Your grade is B.");
+	}else{
+	}
+		if(a<=85&&a>82){
+			System.out.println("Your grade is B-.");
+	}else{
+	}
+		if(a<=82&&a>79){
+			System.out.println("Your grade is C+.");
+	}else{
+	}
+		if(a<=50&&a>53){
+			System.out.println("Your grade is C.");
+	}else{
+	}
+		if(a<=53&&a>56){
+			System.out.println("Your grade is C-.");
+	}else{
+	}
+		if(a<=56&&a>59){
+			System.out.println("Your grade is D+.");
+	}else{
+	}
+		if(a<=59&&a>62){
+			System.out.println("Your grade is D.");
+	}else{
+	}
+		if(a<=62&&a>=60){
+			System.out.println("Your grade is D-.");
+	}else{
+	}
+		if(a<60){
+			System.out.println("Your grade is E.");
+	}
+		
+	}		
+	}
+	}
+	}
