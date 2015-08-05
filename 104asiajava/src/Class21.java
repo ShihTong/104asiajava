9b7a33b19553a3814c605920375bfdf2eb38d489
asdasd950 <o60417o@yahoo.com.tw>
2015-08-05 13:37:02 +0800

    .

--- 104asiajava/src/Class21.java
+++ 104asiajava/src/Class21.java
@@ -1,9 +1,11 @@
-
+import java.util.Scanner;
 public class Class21 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		System.out.println("½Ð¿é¤J¦¨ÁZ");
+		int 
 	}
 
 }
