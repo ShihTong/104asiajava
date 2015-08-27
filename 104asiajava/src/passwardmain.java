fc802eea497e83e37602182f4e07dd99fb97f078
asdasd950 <o60417o@yahoo.com.tw>
2015-08-27 15:37:58 +0800

    2.0

--- 104asiajava/src/passwardmain.java
+++ 104asiajava/src/passwardmain.java
@@ -16,10 +16,10 @@ public class passwardmain extends JFrame {
 	private JScrollPane scroll = new JScrollPane(answer);
 	private JLabel jlb1 = new JLabel();
 	private JLabel jlb2 = new JLabel();
-
+	private JLabel jlb3 = new JLabel();
 	private Container cp;
 	private JFormattedTextField number = new JFormattedTextField();
-	private int A = 0, B = 0;
+	private int A = 0, B = 0, count = 0;
 	private int[] data = new int[4];
 	private int[] user = new int[4];
 
@@ -38,9 +38,11 @@ public class passwardmain extends JFrame {
 		cp.add(jbtnGuess);
 		cp.add(scroll);
 		cp.add(jlb2);
-
+		cp.add(jlb3);
 		jlb1.setBounds(50, 125, 300, 25);
 		jlb1.setText("顯示A為同個位數相同,顯示B為不同位數相同");
+		jlb3.setBounds(250, 25, 300, 25);
+		jlb3.setText("你有10次機會!!!");
 		number.setBounds(30, 50, 150, 25);
 		jbtnGuess.setBounds(250, 50, 100, 25);
 		scroll.setBounds(30, 150, 320, 380);
@@ -49,23 +51,29 @@ public class passwardmain extends JFrame {
 		number.addKeyListener(new KeyAdapter() {
 			public void keyTyped(KeyEvent a) {
 				int keyChar = a.getKeyChar();
-					if (number.getText().length() < 4
-							&& keyChar >= KeyEvent.VK_0
-							&& keyChar <= KeyEvent.VK_9) {
+				if (number.getText().length() < 4 && keyChar >= KeyEvent.VK_0
+						&& keyChar <= KeyEvent.VK_9) {
+				} else {
+					if (keyChar == KeyEvent.VK_BACK_SPACE) {
 					} else {
-						if (keyChar == KeyEvent.VK_BACK_SPACE) {
-						} else {
-							a.consume();
-						}
+						a.consume();
 					}
 				}
-			
+			}
 
 		});
 
 		jbtnGuess.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent a) {
 				if (number.getText().length() == 4) {
+					count++;
+					if (count >= 1) {
+						jlb2.setBounds(200, 25, 300, 25);
+						jlb2.setText("你已經猜了" + count + "次!");
+						jlb2.setText("你已經猜了" + count + "次!" + " " + "剩下"
+								+ (10 - count) + "次機會了!");
+						jlb3.setText("");
+					}
 					int c = Integer.parseInt(number.getText());
 					A = 0;
 					B = 0;
@@ -110,6 +118,21 @@ public class passwardmain extends JFrame {
 					}
 
 					number.setText("");
+					if (count == 10) {
+						Object[] option = { "重來一遍!","認輸了!" };
+						int n = JOptionPane.showOptionDialog(null,
+								"哈哈，你輸了!!!", "你輸了!!!",
+								JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE ,
+								null, option, option[0]);
+						if (n == JOptionPane.YES_OPTION) {
+							count=0;
+							data = rndNum();
+							answer.setText("");
+						} else {
+							System.exit(0);
+						}
+
+					}
 				}
 			}
 		});
@@ -127,34 +150,21 @@ public class passwardmain extends JFrame {
 				"你贏啦!可以吃東西了!\n不知道吃什麼,按是決定吧!", "恭喜!!!",
 				JOptionPane.YES_NO_OPTION);
 		if (n == JOptionPane.YES_OPTION) {
-			String resturant[] = { 
-					"霧峰老牌羊肉吧!\n在霧峰區樹仁路25號",
-					"土耳其美食吧!\n在台中市霧峰區柳豐六街30號", 
-					"弘爺早餐店吧!\n在台中市霧峰區柳豐路428號",
+			String resturant[] = { "霧峰老牌羊肉吧!\n在霧峰區樹仁路25號",
+					"土耳其美食吧!\n在台中市霧峰區柳豐六街30號", "弘爺早餐店吧!\n在台中市霧峰區柳豐路428號",
 					"Pizza Factory 披薩工廠吧!\n在台中市霧峰區樹仁五街12號",
-					"老先覺麻辣窯燒火鍋吧!\n在台中市霧峰區樹仁路45號", 
-					"三顧茅廬麻辣滷味吧!\n在台中市霧峰區樹仁路71號",
+					"老先覺麻辣窯燒火鍋吧!\n在台中市霧峰區樹仁路45號", "三顧茅廬麻辣滷味吧!\n在台中市霧峰區樹仁路71號",
 					"NU PASTA杯杯麵吧!\n在台中市霧峰區樹仁路107號",
 					"O2 Brunch 歐圖早午餐廚房吧!\n在台中市霧峰區樹仁五街6號",
-					"韓國食堂吧!\n在台中市霧峰區林森路998號", 
-					"四海遊龍鍋貼專賣店吧!\n在台中市霧峰區中正路841號",
-					"台北江麻辣臭豆腐專賣舖吧!\n在台中市霧峰區樹仁路56-5號",
-					"大慶麵店吧!\n在台中市霧峰區樹仁路46號",
-					"振卿肉羹大王吧!\n在台中市霧峰區林森路762號",
-					"祥鶴日本料理吧!\n在台中市霧峰區樹仁路39號",
-					"口福牛肉麵吧!\n在台中市霧峰區樹仁路12號", 
-					"狸之屋日式食堂吧!\n在台中市霧峰區柳豐路492號",
-					"廣鎮茶藝館吧!\n在台中市霧峰區林森路998號",
-					"悟饕池上飯包吧!\n在台中市霧峰區中正路1101-2號",
-					"Circus簡餐店吧!\n在台中市霧峰區柳豐六街36號",
-					"拉亞漢堡吧!\n在台中市霧峰區柳豐二街47號",
-					"舞丼吧!\n在台中市霧峰區柳豐路478號",
-					"虎匠拉麵吧!\n在台中市霧峰區柳豐六街26號",
-					"李記涼麵吧!\n在台中市霧峰區柳豐六街18號",
-					"禾家簡餐吧!\n在台中市霧峰區柳豐六街9號",
-					"品軫嗶嗶飯吧!\n在台中市台中市霧峰區柳豐路458號", 
-					"讚不絕口吧!\n在台中市霧峰區柳豐路480號" 
-					};
+					"韓國食堂吧!\n在台中市霧峰區林森路998號", "四海遊龍鍋貼專賣店吧!\n在台中市霧峰區中正路841號",
+					"台北江麻辣臭豆腐專賣舖吧!\n在台中市霧峰區樹仁路56-5號", "大慶麵店吧!\n在台中市霧峰區樹仁路46號",
+					"振卿肉羹大王吧!\n在台中市霧峰區林森路762號", "祥鶴日本料理吧!\n在台中市霧峰區樹仁路39號",
+					"口福牛肉麵吧!\n在台中市霧峰區樹仁路12號", "狸之屋日式食堂吧!\n在台中市霧峰區柳豐路492號",
+					"廣鎮茶藝館吧!\n在台中市霧峰區林森路998號", "悟饕池上飯包吧!\n在台中市霧峰區中正路1101-2號",
+					"Circus簡餐店吧!\n在台中市霧峰區柳豐六街36號", "拉亞漢堡吧!\n在台中市霧峰區柳豐二街47號",
+					"舞丼吧!\n在台中市霧峰區柳豐路478號", "虎匠拉麵吧!\n在台中市霧峰區柳豐六街26號",
+					"李記涼麵吧!\n在台中市霧峰區柳豐六街18號", "禾家簡餐吧!\n在台中市霧峰區柳豐六街9號",
+					"品軫嗶嗶飯吧!\n在台中市台中市霧峰區柳豐路458號", "讚不絕口吧!\n在台中市霧峰區柳豐路480號" };
 			int a = (int) (Math.random() * resturant.length);
 			Object[] options = { "開動了!" };
 			JOptionPane.showOptionDialog(null, "現在去吃" + resturant[a], "吃飯了!",
@@ -179,7 +189,7 @@ public class passwardmain extends JFrame {
 		}
 		// for (int n : num) {
 		// System.out.print(n + "");
-		// }
+		 //}
 		return num;
 	}
 }
